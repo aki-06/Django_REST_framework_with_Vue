c93ddb27b63e4b8f077e8e314015a16c38046544
akihironemoto <akihiro.nemoto.06@gmail.com>
2018-04-15 08:30:36 +0900

    views.py編集

--- employee/views.py
+++ employee/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+import os
 
-# Create your views here.
+from django.conf import settings
+from django.http.response import HttpResponse
+from rest_framework import viewsets
+
+from employee.models import Employee
+from employee.serializer import EmployeeSerializer
+
+def index(_):
+	"""静的ファイルを返すview"""
+	html = open(os.path.join(settings.STATICFILES_DIRS[0], "vue_grid.html")).read()
+	return HttpResponse(html)
+
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+	"""RestAPIのviewsets"""
+	queryset = Employee.objects.all()
+	serializer_class = EmployeeSerializer
+	filter_fields = ("id", "name", "age", "department")
